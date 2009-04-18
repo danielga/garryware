865f971f6e8a822b810b08c33eafe02a6a997048
Hurricaaane <Hurricaaane@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-18 21:37:51 +0000

    wait for next revision
    git-svn-id: http://subversion.assembla.com/svn/garryware@29 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- entities/weapons/gmdm_pistol/shared.lua
+++ entities/weapons/gmdm_pistol/shared.lua
@@ -4,11 +4,11 @@ if ( SERVER ) then
 end
 
 SWEP.Base				= "gmdm_base"
-SWEP.PrintName			= "Pistol"			
+SWEP.PrintName			= "Camera"			
 SWEP.Slot				= 1
 SWEP.SlotPos			= 0
-SWEP.DrawAmmo			= true
-SWEP.DrawCrosshair		= true
+SWEP.DrawAmmo			= false
+SWEP.DrawCrosshair		= false
 SWEP.ViewModel			= "models/weapons/v_pistol.mdl"
 SWEP.WorldModel			= "models/weapons/w_pistol.mdl"
 
@@ -19,33 +19,74 @@ function SWEP:Initialize()
 	
 end
 
-local ShootSound = Sound( "Weapon_Pistol.NPC_Single" )
+local ShootSound = Sound( "NPC_CScanner.TakePhoto" )
 
 SWEP.RunArmAngle  = Angle( 70, 0, 0 )
 SWEP.RunArmOffset = Vector( 25, 4, 0 )
+
+function SWEP:Deploy()
+
+	self.Owner:DrawViewModel( false )
+
+end
 
 /*---------------------------------------------------------
    PRIMARY
    Semi Auto
 ---------------------------------------------------------*/
 
-SWEP.Primary.ClipSize		= 12
-SWEP.Primary.DefaultClip	= 12
+SWEP.Primary.ClipSize		= 2
+SWEP.Primary.DefaultClip	= 2
 SWEP.Primary.Automatic		= false
-SWEP.Primary.Ammo			= "Pistol"
+SWEP.Primary.Ammo			= "none"
 
 function SWEP:PrimaryAttack()
 
-	self.Weapon:SetNextPrimaryFire( CurTime() + 0.1 )
-	self.Weapon:SetNextSecondaryFire( CurTime() + 0.1 )
+	self.Weapon:SetNextPrimaryFire( CurTime() + 0.3 )
+	self.Weapon:SetNextSecondaryFire( CurTime() + 0.3 )
 	
 	if ( !self:CanShootWeapon() ) then return end
 	if ( !self:CanPrimaryAttack() ) then return end
 	
-	self:GMDMShootBullet( 15, ShootSound, math.Rand( -1, -0.5 ), math.Rand( -1, 1 ) )
+	if (SERVER) then
+		self.Weapon:SetNWFloat("phototick",CurTime())
+		for k,target in pairs(team.GetPlayers(TEAM_UNASSIGNED)) do 
+			
+			local direction = (target:GetPos() - self.Owner:GetShootPos()):GetNormalized()
+			local aiming  = self.Owner:GetAimVector()
+			if (direction:DotProduct(aiming) > math.acos(math.rad(34)) && target != self.Owner) then
+				local pos = self.Owner:GetShootPos()
+				local targetpo = self.Owner:GetAimVector()
+				local tracedata = {}
+				tracedata.start = self.Owner:GetShootPos()
+				tracedata.endpos = target:GetPos()
+				tracedata.filter = {self.Owner , target}
+				local trace = util.TraceLine(tracedata)
+				if trace.Hit == false then
+					GAMEMODE:WarePlayerDestinyLose( target )
+				end 
+			end
+		end
+	end
 	
 	self:TakePrimaryAmmo( 1 )
+	self.Weapon:EmitSound( ShootSound )
 
+end
+
+function SWEP:DrawHUD()
+	surface.SetDrawColor(0,0,0,255)
+	surface.DrawRect( 0, 0, ScrW(), ScrH()*0.1 )
+	surface.DrawRect( 0, ScrH() - ScrH()*0.1, ScrW(), ScrH()*0.1 )
+	
+	surface.DrawRect( 0, ScrH()*0.1 ,ScrW()*0.1, ScrH() - ScrH()*0.2 )
+	surface.DrawRect( ScrW() - ScrW()*0.1, ScrH()*0.1 ,ScrW()*0.1, ScrH() - ScrH()*0.2 )
+	
+	local phototick = self.Weapon:GetNWFloat("phototick",0)
+	if (CurTime() - phototick < 0.7) then
+		surface.SetDrawColor(255,255,255,255 - math.Clamp(255*(CurTime() - phototick)/0.7,0,255))
+		surface.DrawRect( 0, 0, ScrW(), ScrH() )
+	end
 end
 
 
@@ -63,5 +104,7 @@ function SWEP:SecondaryAttack()
 	return false
 end
 
-
+function SWEP:TranslateFOV()
+	return 40
+end
 
