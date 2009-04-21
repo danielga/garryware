88810a677d47c731f3db258e2a3798468a3baf0e
kelth <kelth@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-21 16:49:06 +0000

    "Try to climb on the boxes" minigame and minor changes
    git-svn-id: http://subversion.assembla.com/svn/garryware@49 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- gamemode/wareminigames/kelthgames.lua
+++ gamemode/wareminigames/kelthgames.lua
@@ -1,20 +1,23 @@
 -- CLIMB ON THE BOXES MINIGAME
 
-registerMinigame("climb",function(self, args)
+registerMinigame("climb",
+function(self, args)
 	--Start of INIT
 	GAMEMODE:SetWareWindupAndLength(1.5,3)
 	
 	GAMEMODE:DrawPlayersTextAndInitialStatus("Climb on the boxes!",0)
 	return
 	
-end,function(self, args)
+end,
+function(self, args)
 	--Start of ACT
 	for k,v in pairs(team.GetPlayers(TEAM_UNASSIGNED)) do 
 		v:Give( "ware_weap_crowbar" )
 	end
 	return
 end)
-registerTrigger("climb","Think",function()  
+registerTrigger("climb","Think",
+function()  
 	for k,v in pairs(team.GetPlayers(TEAM_UNASSIGNED)) do 
 		v:SetAchievedNoDestiny(0)
 	end
@@ -31,7 +34,8 @@ end)
 
 -- TOUCH SKY MINIGAME
 
-registerMinigame("touchsky",function(self, args)
+registerMinigame("touchsky",
+function(self, args)
 	--Start of INIT
 	GAMEMODE:SetWareWindupAndLength(2,6)
 	GAMEMODE:DrawPlayersTextAndInitialStatus("Stay on the ground ! ",1)
@@ -40,7 +44,8 @@ registerMinigame("touchsky",function(self, args)
 	GAMEMODE.GamePool.zsky = (entover[1]:GetPos().z + entsky[1]:GetPos().z)/2
 	return
 	
-end,function(self, args)
+end,
+function(self, args)
 	--Start of ACT
 	GAMEMODE:DrawPlayersTextAndInitialStatus("Don't touch the sky ! ",1)
 	
@@ -48,16 +53,19 @@ end,function(self, args)
 		v:Give( "ware_velocitygun" )
 	end
 	for k,v in pairs(player.GetAll()) do 
+		v:SetGroundEntity( NULL )
 		v:SetGravity(-0.1)
 	end
 	return
-end,function(self, args)
+end,
+function(self, args)
 	-- End of ACT
 	for k,v in pairs(player.GetAll()) do 
 		v:SetGravity(1)
 	end
 end)
-registerTrigger("touchsky","Think",function()  
+registerTrigger("touchsky","Think",
+function()  
 	for k,v in pairs(team.GetPlayers(TEAM_UNASSIGNED)) do 
 		if v:GetPos().z > GAMEMODE.GamePool.zsky then
 			GAMEMODE:WarePlayerDestinyLose( v )
@@ -68,13 +76,15 @@ end)
 
 -- CATCH A BALL MINIGAME
 
-registerMinigame("catchball",function(self, args)
+registerMinigame("catchball",
+function(self, args)
 	--Start of INIT
 	GAMEMODE:SetWareWindupAndLength(2,6)
 	GAMEMODE:DrawPlayersTextAndInitialStatus("Catch a ball !",0)
 	return
 	
-end,function(self, args)
+end,
+function(self, args)
 	--Start of ACT
 	local entposcopy = table.Copy(GAMEMODE:GetEnts(ENTS_ONCRATE)) --Copying the table, and the removing elements from it
 	local numberSpawns = math.Clamp(math.ceil(team.NumPlayers(TEAM_UNASSIGNED)*0.5),1,table.Count(entposcopy))
@@ -102,71 +112,59 @@ end,function(self, args)
 	end
 	
 	return
-end,function(self, args)
+end,
+function(self, args)
 	-- End of ACT
 
 end)
 
-registerTrigger("catchball","GravGunOnPickedUp",function(pl, ent)  
-	GAMEMODE:WarePlayerDestinyWin( pl )
+registerTrigger("catchball","GravGunOnPickedUp",
+function(pl, ent)  
+	if ent:GetClass() == "ware_catchball" then
+		GAMEMODE:WarePlayerDestinyWin( pl )
+		ent:Remove()
+		pl:StripWeapons()
+	end
 end)
 
--- CROUCH MINIGAME
+-- TRY TO CLIMB ON THE BOXES MINIGAME
 
---Useful function to spawn a crouchball
-/*
-function WARESpawnCrouchBall(pos)
-	local ent = ents.Create ("ware_crouchball");
-	ent:SetPos(pos);
-	ent:Spawn();
-	GAMEMODE:AppendEntToBin(ent)
-	GAMEMODE:MakeAppearEffect(ent:GetPos())
-	local crouch = math.random(0,4)
-	
-	if crouch >= 3 then 
-		ent:SetColor(math.random(100,255),math.random(100,255),0,255)
-		ent:SetNWBool("crouch", false)
-	else
-		ent:SetColor(0,0,255,255)
-		ent:SetNWBool("crouch", true)
-	end
-	mess=GAMEMODE.GamePool.Fake[math.random(1,table.Count(GAMEMODE.GamePool.Fake))]
-	for k,v in pairs(team.GetPlayers(TEAM_UNASSIGNED)) do 
-		v:PrintMessage( HUD_PRINTCENTER, mess )
-	end
-	
-	
-	return
-end
-
-registerMinigame("crouch",function(self, args)
+registerMinigame("tryclimb",
+function(self, args)
 	--Start of INIT
-	GAMEMODE.GamePool.NbBall = math.random(4,8)
-	GAMEMODE.GamePool.Fake = {"To crouch or no to crouch ?","I think you should crouch...","Crouch !","Look ! It's a blue one !","You may have to crouch next time.","Don't crouch this time !"}
-	GAMEMODE:SetWareWindupAndLength( 4, GAMEMODE.GamePool.NbBall/2 + 2 )
-	GAMEMODE:DrawPlayersTextAndInitialStatus("Crouch only when a blue ball hit the ground !",1)
+	GAMEMODE:SetWareWindupAndLength(2,6)
+	GAMEMODE:DrawPlayersTextAndInitialStatus("Try to climb on the boxes !",0)
 	return
 	
-end,function(self, args)
+end,
+function(self, args)
 	--Start of ACT
-	local entposcopy = table.Copy(GAMEMODE:GetEnts(ENTS_INAIR)) --Copying the table, and the removing elements from it
-
-	local posCenter = Vector(0,0,0)
-	for k,v in pairs(entposcopy) do
-		posCenter = posCenter + v:GetPos()	
-		table.remove( entposcopy, k )
+	for k,v in pairs(team.GetPlayers(TEAM_UNASSIGNED)) do 
+		v:SetEyeAngles( Angle( 0,0,180 ) )
 	end
-	posCenter = posCenter/table.Count(entposcopy)
-	
-	for i = 0, GAMEMODE.GamePool.NbBall do
-		timer.Simple(i/2,WARESpawnCrouchBall,posCenter)
-	end
-	
 	
 	return
-end,function(self, args)
-	-- End of ACT
-
+end,
+function(self, args)
+	-- End of Act
+	for k,v in pairs(player.GetAll()) do 
+		v:SetEyeAngles( Angle(0,0,0))
+	end
 end)
-*/
+registerTrigger("tryclimb","Think",
+function() 
+	for k,v in pairs(team.GetPlayers(TEAM_UNASSIGNED)) do 
+		v:SetAchievedNoDestiny(0)
+		
+	end
+	local entposcopy = 	table.Copy(GAMEMODE:GetEnts(ENTS_ONCRATE))
+	for _,block in pairs(entposcopy) do
+		local box = ents.FindInBox(block:GetPos()+Vector(-30,-30,0),block:GetPos()+Vector(30,30,64))
+		for _,target in pairs(box) do
+			if target:IsPlayer() then
+				target:SetAchievedNoDestiny(1)
+			end
+		end
+	end
+end)
 
