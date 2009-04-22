93c4eb1baa27bf1553e6f3b919cf2bab959d6518
Hurricaaane <Hurricaaane@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-22 12:07:07 +0000

    Improved "Where was it ?" landmark effect.
    git-svn-id: http://subversion.assembla.com/svn/garryware@57 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- entities/effects/ware_landmark/init.lua
+++ entities/effects/ware_landmark/init.lua
@@ -5,23 +5,23 @@ function EFFECT:Init( data )
 	
 	local emitter = ParticleEmitter( self.Pos )
 	
-	for i=1,100 do
-		local particle = emitter:Add( "effects/blueflare1", self.Pos + VectorRand() * 5)
-		particle:SetColor(0,0,250)
-		particle:SetStartSize( math.Rand(10,15) )
+	for i=1,50 do
+		local particle = emitter:Add( "effects/yellowflare", self.Pos + VectorRand() * 5)
+		particle:SetColor(255,255,0)
+		particle:SetStartSize( math.Rand(20,30) )
 		particle:SetEndSize( 0 )
 		particle:SetStartAlpha( 250 )
 		particle:SetEndAlpha( 0 )
 		particle:SetDieTime( 2.7 )
-		particle:SetVelocity( VectorRand() * 10 + Vector(0,0,(math.random(0,2)-1)*math.random(20,100)) )
+		particle:SetVelocity( VectorRand() * 20 + Vector(0,0,(math.random(0,2)-1)*math.random(20,100)) )
 		
 		particle:SetBounce(0.8)
 		particle:SetGravity( Vector( 0, 0, 0 ) )
 		particle:SetCollide(true)
 	end
 	
-	local particle = emitter:Add( "effects/blueflare1", self.Pos)
-	particle:SetColor(0,0,250)
+	local particle = emitter:Add( "effects/yellowflare", self.Pos)
+	particle:SetColor(255,255,0)
 	particle:SetStartSize( 20 )
 	particle:SetEndSize( 0 )
 	particle:SetStartAlpha( 250 )
