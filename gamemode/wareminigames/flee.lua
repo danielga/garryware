61cac4a7409ea9cb8485765478cb3028418eb1a7
Hurricaaane <Hurricaaane@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-25 15:22:50 +0000

    Added minigame "Flee !" by Ha3
    git-svn-id: http://subversion.assembla.com/svn/garryware@96 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ gamemode/wareminigames/flee.lua
@@ -0,0 +1,63 @@
+WARE.Author = "Hurricaaane (Ha3)"
+
+local possiblenpcs = { "npc_manhack" , "npc_rollermine" }
+
+function WARE:Initialize()
+	WARE.Choice = possiblenpcs[math.random(1,2)]
+	if WARE.Choice == "npc_rollermine" then
+		GAMEMODE:DrawPlayersTextAndInitialStatus("Don't be on the boxes !",1)
+		GAMEMODE:SetWareWindupAndLength(3,8)
+	else
+		GAMEMODE:DrawPlayersTextAndInitialStatus("Flee !",1)
+		GAMEMODE:SetWareWindupAndLength(1,8)
+	end
+	
+	return
+end
+
+function WARE:StartAction()
+	if WARE.Choice == "npc_rollermine" then
+		GAMEMODE:DrawPlayersTextAndInitialStatus("Flee !",1)
+	end
+
+	local ratio = 0.4
+	local minimum = 3
+	local num = math.Clamp(math.ceil(team.NumPlayers(TEAM_UNASSIGNED)*ratio),minimum,64)
+	local entposcopy = GAMEMODE:GetRandomLocations(num, ENTS_INAIR)
+	for k,v in pairs(entposcopy) do
+		local ent = ents.Create (WARE.Choice);
+		ent:SetPos(v:GetPos());
+		ent:Spawn();
+		
+		local phys = ent:GetPhysicsObject()
+		phys:ApplyForceCenter (VectorRand() * 16);
+		
+		GAMEMODE:AppendEntToBin(ent)
+		GAMEMODE:MakeAppearEffect(ent:GetPos())
+	end
+	return
+end
+
+function WARE:EndAction()
+
+end
+
+function WARE:EntityTakeDamage( ent, inflictor, attacker, amount)
+	if ent:IsPlayer() && attacker:IsNPC( ) then
+		ent:WarePlayerDestinyLose( )
+	end
+end
+
+function WARE:Think( )
+	if WARE.Choice == "npc_rollermine" then
+		local entposcopy = 	table.Copy(GAMEMODE:GetEnts(ENTS_ONCRATE))
+		for _,block in pairs(entposcopy) do
+			local box = ents.FindInBox(block:GetPos()+Vector(-30,-30,0),block:GetPos()+Vector(30,30,64))
+			for _,target in pairs(box) do
+				if target:IsPlayer() then
+					target:WarePlayerDestinyLose( )
+				end
+			end
+		end
+	end
+end
