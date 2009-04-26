8fa7ffb3f04d7c78819b11971ea2e3c83b018085
Hurricaaane <Hurricaaane@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-26 12:20:59 +0000

    Updated flee to work in empty
    git-svn-id: http://subversion.assembla.com/svn/garryware@108 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- gamemode/wareminigames/flee.lua
+++ gamemode/wareminigames/flee.lua
@@ -1,29 +1,23 @@
 WARE.Author = "Hurricaaane (Ha3)"
+WARE.Room = "empty"
 
 local possiblenpcs = { "npc_manhack" , "npc_rollermine" }
 
 function WARE:Initialize()
 	self.Choice = possiblenpcs[math.random(1,2)]
-	if self.Choice == "npc_rollermine" then
-		GAMEMODE:DrawPlayersTextAndInitialStatus("Don't be on the boxes !",1)
-		GAMEMODE:SetWareWindupAndLength(3,8)
-	else
-		GAMEMODE:DrawPlayersTextAndInitialStatus("Flee !",1)
-		GAMEMODE:SetWareWindupAndLength(1,8)
-	end
+	GAMEMODE:DrawPlayersTextAndInitialStatus("Flee !",1)
+	GAMEMODE:SetWareWindupAndLength(2,8)
 	
 	return
 end
 
 function WARE:StartAction()
-	if self.Choice == "npc_rollermine" then
-		GAMEMODE:DrawPlayersTextAndInitialStatus("Flee !",1)
-	end
+	GAMEMODE:DrawPlayersTextAndInitialStatus("Flee !",1)
 
-	local ratio = 0.4
+	local ratio = 0.7
 	local minimum = 3
 	local num = math.Clamp(math.ceil(team.NumPlayers(TEAM_UNASSIGNED)*ratio),minimum,64)
-	local entposcopy = GAMEMODE:GetRandomLocations(num, ENTS_INAIR)
+	local entposcopy = GAMEMODE:GetRandomLocations(num, {"dark_inair","light_inair"})
 	for k,v in pairs(entposcopy) do
 		local ent = ents.Create (self.Choice);
 		ent:SetPos(v:GetPos());
@@ -45,19 +39,5 @@ end
 function WARE:EntityTakeDamage( ent, inflictor, attacker, amount)
 	if ent:IsPlayer() && attacker:IsNPC( ) then
 		ent:WarePlayerDestinyLose( )
-	end
-end
-
-function WARE:Think( )
-	if self.Choice == "npc_rollermine" then
-		local entposcopy = 	table.Copy(GAMEMODE:GetEnts(ENTS_ONCRATE))
-		for _,block in pairs(entposcopy) do
-			local box = ents.FindInBox(block:GetPos()+Vector(-30,-30,0),block:GetPos()+Vector(30,30,64))
-			for _,target in pairs(box) do
-				if target:IsPlayer() then
-					target:WarePlayerDestinyLose( )
-				end
-			end
-		end
 	end
 end
