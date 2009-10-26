e79a1fac131789821db4f7dbee4c9887b9139951
Hurricaaane <Hurricaaane@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-10-26 02:10:34 +0000

    ____________________________________________
    Wipe out for the New Version of Garry Ware !
    ____________________________________________
    
    git-svn-id: http://subversion.assembla.com/svn/garryware@172 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- entities/entities/gmod_landmarkonremove/init.lua
+++
@@ -1,5 +0,0 @@
-ENT.Type = "point"
-
-function ENT:OnRemove( )
-	GAMEMODE:MakeLandmarkEffect(self:GetPos())
-end
--- entities/entities/gmod_warelocation/init.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type 			= "anim"
-ENT.Base 			= "base_anim"
-
-function ENT:Initialize()
-	self.Entity:PhysicsInitBox(Vector(-1,-1,-1), Vector(1,1,1))
-	self.Entity:SetCollisionBounds(Vector(-1,-1,-1), Vector(1,1,1))
-	self.Entity:SetNoDraw(true)
-	self.Entity:SetMoveType(MOVETYPE_NONE)
-end
