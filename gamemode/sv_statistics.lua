63d5cd48eb7071b32ecb39e24f337e4e594f6de2
Hurricaaane <Hurricaaane@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-05-03 00:50:05 +0000

    Trying to do the stream thing
    git-svn-id: http://subversion.assembla.com/svn/garryware@306 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- gamemode/sv_statistics.lua
+++ gamemode/sv_statistics.lua
@@ -46,7 +46,7 @@ function GM:StatsUpdateMinigameInfo( )
 		end
 	end
 	
-	local sMinigameName = self.Minigame.Name
+	--local sMinigameName = self.Minigame.Name
 	local iTokens = 0
 	-- TOKEN_GW_STATS : Why do we use a function ?
 	-- That's because tokens can vary with the different phases
@@ -56,9 +56,60 @@ function GM:StatsUpdateMinigameInfo( )
 		iTokens = self:StatsRawTableToBitwise( self.Minigame:GetTokens() or nil )
 	end
 	
-	local minigamename = self.Minigame.Name
-	local data = { sMinigameName, iWare, iPhase, WonLostUIDS, iTokens }
+	local sMinigameDesc = self.Minigame.Desc
+	local data = { sMinigameDesc, iWare, iPhase, WonLostUIDS, iTokens }
 	
 	table.insert( stats.PlayTable, data )
+	
+end
+
+-- TOKEN_GW_STATS : SHOULD CALL AFTER REMOVEUNPLAYABLE IN INIT
+function StatsPoolMinigameDescriptions()
+	local names = ware_mod.GetNamesTable()
+	for _,name in pairs( names ) do
+		local desc = ware_mod.Get(v).Desc
+		
+		if desc then
+			umsg.PoolString( desc )
+		end
+	end
+	
+end
+
+function GM:StatsStream()
+	-- HOLY JEEBUS.
+	--Send players
+	for uid,data in pairs( stats.PlayerTable ) do
+		-- TOKEN_GW_STATS : Optimize
+		umsg.Start("StatsPlayerData")
+			umsg.Char( uid - 128 )
+			umsg.String( data.Nick )
+		umsg.End()
+		
+	end
+	
+	-- AT THE SAME TIME, TRY TO MAKE THE PLAYER CRASH,
+	-- DUE TO UNOPTIMIZED CODE IN EDGE CASES.
+	--Send minigames
+	for i,data in pairs( stats.PlayTable ) do
+		umsg.Start("ModelList")
+			umsg.Char( i - 128 )
+			umsg.String( data[1] )
+			umsg.Char( data[2] - 128 )
+			umsg.Char( data[3] - 128 )
+			umsg.Char( #data[4][1] - 128 )
+			umsg.Char( #data[4][2] - 128 )
+			umsg.Char( #data[4][3] - 128 )
+			for l,tp in pairs( data[4] ) do
+				for k,uid in pairs( tp[l] ) do
+					-- Remember, there actually could be 32 of these.
+					-- I mean, does chat.AddText use more?
+					umsg.Char( uid - 128 )
+				end
+			end
+			-- TOKEN_GW_STATS : Send the bitwise thing or try to use Pooled Strings.
+			umsg.Short( #data[5] - 128 )
+		umsg.End()
+	end
 	
 end
